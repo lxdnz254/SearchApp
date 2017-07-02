c375d2f4b6e8b0d2e4d25c21492987850178db26
lxdnz254 <lxdnz34@gmail.com>
2017-07-02 16:05:00 +1200

    Update to heroku web

--- src/db/bolt.js
+++ src/db/bolt.js
@@ -1,5 +1,10 @@
 var neo4j = window.neo4j.v1;
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "neo4j2"));
+
+var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
+var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
+var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
+
+var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
 
 // Register a callback to know if driver creation failed.
 // This could happen due to wrong credentials or database unavailability:
